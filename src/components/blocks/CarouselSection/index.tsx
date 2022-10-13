4c408aee8d6e44933c6358b5be319cfdfb87c120
RehkMansa <godspowernathaniel25@gmail.com>
2022-10-13 02:52:17 +0100

    added carousel

---
+++ src/components/blocks/CarouselSection/index.tsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/no-array-index-key */
+import { ReactNode } from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+
+type Props = { imageArr: { img: string; children?: ReactNode }[] };
+
+const CarouselSection = ({ imageArr }: Props) => (
+    <Carousel>
+        {imageArr.map((arr, idx) => (
+            <div key={idx}>
+                <img src={arr.img} alt="" />
+                {arr.children}
+            </div>
+        ))}
+    </Carousel>
+);
+
+export default CarouselSection;
