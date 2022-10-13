8f1963599a7b51ce0b4e16648f176d626ebdbaf8
RehkMansa <godspowernathaniel25@gmail.com>
2022-10-13 03:59:46 +0100

    made carouel swipeable

--- src/components/blocks/CarouselSection/index.tsx
+++ src/components/blocks/CarouselSection/index.tsx
@@ -1,12 +1,11 @@
-/* eslint-disable react/no-array-index-key */
 import { ReactNode } from "react";
 import { Carousel } from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 type Props = { imageArr: { img: string; children?: ReactNode; idx: number }[] };
 
 const CarouselSection = ({ imageArr }: Props) => (
-    <Carousel showThumbs={false} autoPlay={false}>
+    <Carousel swipeable={true} showThumbs={false} autoPlay={true}>
         {imageArr.map(arr => (
             <div key={arr.idx}>
                 <img src={arr.img} alt="" />
