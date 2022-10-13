ccfb3e644dd67c8f073f21f5697c86c7f8fbbd9f
RehkMansa <godspowernathaniel25@gmail.com>
2022-10-13 03:33:30 +0100

    fixed carousel issues

--- src/components/blocks/CarouselSection/index.tsx
+++ src/components/blocks/CarouselSection/index.tsx
@@ -3,12 +3,12 @@ import { ReactNode } from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 
-type Props = { imageArr: { img: string; children?: ReactNode }[] };
+type Props = { imageArr: { img: string; children?: ReactNode; idx: number }[] };
 
 const CarouselSection = ({ imageArr }: Props) => (
-    <Carousel>
-        {imageArr.map((arr, idx) => (
-            <div key={idx}>
+    <Carousel showThumbs={false} autoPlay={false}>
+        {imageArr.map(arr => (
+            <div key={arr.idx}>
                 <img src={arr.img} alt="" />
                 {arr.children}
             </div>
--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -1,83 +1,84 @@
-const Homepage = () => (
-    <header className="header slider-fade">
-        <div className="owl-carousel owl-theme">
-            <div
-                className="text-right item bg-img"
-                data-overlay-dark="3"
-                data-background="img/slider/1.jpg"
-            >
-                <div className="v-middle caption mt-30">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-md-7 offset-md-5">
-                                <h1>Innovate Desing in Toronto</h1>
-                                <p>
-                                    Architecture viverra tellus nec massa dictum the
-                                    euismoe.
-                                    <br />
-                                    Curabitur viverra the posuere aukue velit.
-                                </p>
-                                <div className="butn-light mt-30 mb-30">
-                                    <a href="/">
-                                        <span>Buy Now</span>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div
-                className="text-right item bg-img"
-                data-overlay-dark="4"
-                data-background="img/slider/2.jpg"
-            >
-                <div className="v-middle caption mt-30">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-md-7 offset-md-5">
-                                <h1>Innovate Desing in Canada</h1>
-                                <p>
-                                    Architecture viverra tellus nec massa dictum the
-                                    euismoe.
-                                    <br />
-                                    Curabitur viverra the posuere aukue velit.
-                                </p>
-                                <div className="butn-light mt-30 mb-30">
-                                    <a href="/">
-                                        <span>Buy Now</span>
-                                    </a>
-                                </div>
-                            </div>
+import styled from "styled-components";
+import Slider1 from "../../assets/img/slider/1.jpg";
+import Slider2 from "../../assets/img/slider/2.jpg";
+import CarouselSection from "../../components/blocks/CarouselSection";
+
+const Wrapper = styled.div`
+    height: 100vh;
+    overflow: hidden;
+
+    @media screen and (min-width: 100px) {
+        & .carousel .slider-wrapper {
+            height: 100vh;
+
+            & img {
+                height: 100vh;
+                object-fit: cover;
+            }
+        }
+    }
+
+    @media screen and (max-width: 500px) {
+        height: 90vh;
+
+        & .carousel .slider-wrapper {
+            height: 90vh;
+
+            & img {
+                height: 90vh;
+            }
+        }
+    }
+`;
+
+const CarouselItem = () => (
+    <div className="text-right item bg-img" data-overlay-dark="4">
+        <div className="v-middle caption mt-30">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-7 offset-md-5">
+                        <h1>Innovate Desing in Canada</h1>
+                        <p>
+                            Architecture viverra tellus nec massa dictum the euismoe.
+                            <br />
+                            Curabitur viverra the posuere aukue velit.
+                        </p>
+                        <div className="butn-light mt-30 mb-30">
+                            <a href="/">
+                                <span>Buy Now</span>
+                            </a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
+);
 
-        <div className="hero-corner" />
-        <div className="hero-corner3" />
+const carouselItems = [
+    {
+        idx: 0,
+        img: Slider1,
+        children: <CarouselItem />,
+    },
+    {
+        idx: 1,
+        img: Slider2,
+        children: <CarouselItem />,
+    },
+];
+
+const Homepage = () => (
+    <header className="header slider-fade">
+        <Wrapper className="">
+            <CarouselSection imageArr={carouselItems} />
+        </Wrapper>
 
         <div className="left-panel">
             <ul className="social-left clearfix">
                 <li>
-                    <a href="index.html#">
+                    <a href="/">
                         <i className="ti-pinterest" />
-                    </a>
-                </li>
-                <li>
-                    <a href="index.html#">
-                        <i className="ti-instagram" />
-                    </a>
-                </li>
-                <li>
-                    <a href="index.html#">
-                        <i className="ti-twitter" />
-                    </a>
-                </li>
-                <li>
-                    <a href="index.html#">
-                        <i className="ti-facebook" />
                     </a>
                 </li>
             </ul>
