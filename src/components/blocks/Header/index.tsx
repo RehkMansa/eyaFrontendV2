61e4bd67398e105dfe113acb7ea23f7ace6da329
RehkMansa <godspowernathaniel25@gmail.com>
2022-10-15 02:09:05 +0100

    added navbar toggle

--- src/components/blocks/Header/index.tsx
+++ src/components/blocks/Header/index.tsx
@@ -1,4 +1,26 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+
+const Button = styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 40px;
+    width: 40px;
+`;
+
+const Line = styled.div`
+    border-block: 1px solid;
+    height: 6px;
+    width: 20px;
+
+    &:last-child {
+        margin-top: 4px;
+        border-bottom: 0;
+        height: 2px;
+    }
+`;
 
 const MenuLink = ({ name, to }: { name: string; to: string }) => (
     <li className="nav-item">
@@ -31,33 +53,45 @@ const menuItems = [
     },
 ];
 
-const Header = () => (
-    <section>
-        <nav className="navbar navbar-expand-lg">
-            <div className="logo-wrapper valign">
-                <div className="logo">
-                    <a href="index.html">
-                        {/* <img src="img/logo.png" className="logo-img" alt="" /> */}
-                        <h2>
-                            MyEya<span>Estate</span>
-                        </h2>
-                    </a>
+const Header = () => {
+    const [show, setShow] = useState(false);
+
+    return (
+        <section>
+            <nav className="navbar navbar-expand-lg">
+                <div className="logo-wrapper valign">
+                    <div className="logo">
+                        <a href="index.html">
+                            {/* <img src="img/logo.png" className="logo-img" alt="" /> */}
+                            <h2>
+                                MyEya<span>Estate</span>
+                            </h2>
+                        </a>
+                    </div>
                 </div>
-            </div>
-            <button className="navbar-toggler" type="button">
-                <span className="icon-bar">
-                    <i className="ti-line-double" />
-                </span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav ml-auto">
-                    {menuItems.map(item => (
-                        <MenuLink key={item.idx} {...item} />
-                    ))}
-                </ul>
-            </div>
-        </nav>
-    </section>
-);
+                <Button
+                    onClick={() => setShow(!show)}
+                    className="navbar-toggler"
+                    type="button"
+                >
+                    <span className="icon-bar">
+                        <Line />
+                        <Line />
+                    </span>
+                </Button>
+                <div
+                    className={`collapse navbar-collapse ${show && "show"}`}
+                    id="navbarSupportedContent"
+                >
+                    <ul className="navbar-nav ml-auto">
+                        {menuItems.map(item => (
+                            <MenuLink key={item.idx} {...item} />
+                        ))}
+                    </ul>
+                </div>
+            </nav>
+        </section>
+    );
+};
 
 export default Header;
