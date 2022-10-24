d53392dc122f46917dcab040f2c7cd48a9d8d8e4
RehkMansa <godspowernathaniel25@gmail.com>
2022-10-24 19:24:40 +0100

    added carousel for programs

--- src/components/sections/Programs/index.tsx
+++ src/components/sections/Programs/index.tsx
@@ -1,14 +1,22 @@
+import { SwiperSlide } from "swiper/react";
+import SwiperSection from "../../blocks/SwiperSection";
 import RewardsImg from "../../../assets/img/eyahomes/8.jpg";
 import HouseDeals from "../../../assets/img/eyahomes/6.jpg";
+import Partnership from "../../../assets/img/eyahomes/7.jpg";
+import styled from "styled-components";
 
 type Props = { name: string; img: string; meta: string };
 
+const Wrapper = styled.div`
+    max-height: 360px;
+`;
+
 const SingleProgram = ({ name, img, meta }: Props) => (
-    <div className="col-md-6">
+    <div className="">
         <div className="item">
-            <div className="position-re o-hidden">
+            <Wrapper className="position-re o-hidden">
                 <img src={img} alt="" />
-            </div>
+            </Wrapper>
             <div className="con">
                 <span className="category">{meta}</span>
                 <h5>
@@ -18,6 +26,27 @@ const SingleProgram = ({ name, img, meta }: Props) => (
         </div>
     </div>
 );
+
+const swiperOptions = [
+    {
+        idx: 0,
+        meta: "500k cash grab for our top agent",
+        img: RewardsImg,
+        name: "Monthly rewards for agents",
+    },
+    {
+        idx: 1,
+        meta: "Affordable deals on all properties",
+        img: HouseDeals,
+        name: "Monthly rewards for agents",
+    },
+    {
+        idx: 2,
+        meta: "Become a partner",
+        img: Partnership,
+        name: "Partnership deal",
+    },
+];
 
 const Programs = () => (
     <section className="eyahomes-blog section-padding">
@@ -29,17 +58,18 @@ const Programs = () => (
                     </h2>
                 </div>
             </div>
-            <div className="row">
-                <SingleProgram
-                    meta="Win monthly prices of upto 500k for every property deal"
-                    img={RewardsImg}
-                    name="Monthly rewards for agents"
-                />
-                <SingleProgram
-                    meta="Win monthly prices of upto 500k for every property deal"
-                    img={HouseDeals}
-                    name="Monthly rewards for agents"
-                />
+            <div className="">
+                <SwiperSection mobileSlides={2} slidesPerView={2}>
+                    {swiperOptions.map(item => (
+                        <SwiperSlide key={item.idx}>
+                            <SingleProgram
+                                meta={item.meta}
+                                img={item.img}
+                                name={item.name}
+                            />
+                        </SwiperSlide>
+                    ))}
+                </SwiperSection>
             </div>
         </div>
     </section>
